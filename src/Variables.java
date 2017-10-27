5a916a41c93e6f3609d64540df55341043dae81b
Yael Brown <yaelrbrown@gmail.com>
2017-10-27 12:51:30 -0400

    Update
    Half-way thru variables.

--- src/Variables.java
+++ src/Variables.java
@@ -41,14 +41,50 @@ public class Variables {
 	 *  		
 	 *  		int - 32-bit integer. (min: -2^31, max: 2^31-1)
 	 *  			
-	 *  		long -   
+	 *  		long - 64-bit integer. (min: -2^63, max 2^63-1)
+	 *  			Use this data type when you need a range of values wider than int. 
 	 *  
-	 *  		float - 
+	 *  		float - 32-bit IEEE 754 floating point. 
+	 *  			Used to save memory in large arrays of floating point numbers.
+	 *  			Should not be used for currency. (use java.math.BigDecimal class instead). 
 	 *  
-	 *  		double - 
+	 *  		double - 64-bit IEEE 754 floating point. 
+	 *  			For decimal values, this data type is generally the default choice. 
+	 *  			Should not be used for currency. 
 	 *  
-	 *  		boolean -
+	 *  		boolean - only 2 values, true and false. Size is not precisely defined.
 	 *  
-	 *  		char -
+	 *  		char - single 16-bit Unicode character. (min: '\u0000' (or 0), max: '\uffff' (or 65,535 inclus.))
+	 *  
+	 *  	Java provides special support for character Strings. (java.lang.String). 
+	 *  		Enclosing text with double quotes automaticaly create a new string object. 
+	 *  
 	 */
+	
+	/*
+	 * Default Values
+	 * 		Relying on default values is generally considered bad programming style. 
+	 * 		
+	 * 		byte 		0
+	 * 		short		0
+	 * 		int 		0
+	 * 		long		0L
+	 * 		float 		0.0f
+	 * 		double		0.0d
+	 * 		char		'\u0000'
+	 * 		boolean		false
+	 * 		String 		null
+	 * 
+	 */
+
+	// Literal is the source code representation of a fixed value. 
+	boolean result = true;
+	char capitalC = 'C';
+	byte b = 100;
+	short s = 10000;
+	int i = 100;
+	
+	
 }
+	
+	
